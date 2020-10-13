9407dcfce6d88cb3db8d0a863fda009e8089d74e
zhangzhenQuan <1726891762@qq.com>
2020-10-13 16:05:59 +0800

    '初始化项目'

---
+++ src/main/java/com/whoamizq/rabbitmqmail/RabbitmqMailApplication.java
@@ -0,0 +1,13 @@
+package com.whoamizq.rabbitmqmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whoamizq/rabbitmqmail/RabbitmqMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whoamizq.rabbitmqmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
