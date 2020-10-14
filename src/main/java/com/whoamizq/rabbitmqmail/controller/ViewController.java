39890dd6bc2b5545f0acb08aa91e9d2c9d85fcf9
zhangzhenQuan <1726891762@qq.com>
2020-10-14 18:11:14 +0800

    添加ViewController

---
+++ src/main/java/com/whoamizq/rabbitmqmail/controller/ViewController.java
@@ -0,0 +1,12 @@
+package com.whoamizq.rabbitmqmail.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping(value = "home")
+    public String index(){
+        return "index";
+    }
+}
