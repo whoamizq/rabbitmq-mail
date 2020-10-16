329da7647c918ddf4e151100853355454dee1012
zhangzhenQuan <1726891762@qq.com>
2020-10-16 17:57:50 +0800

    添加tokenController

---
+++ src/main/java/com/whoamizq/rabbitmqmail/controller/TokenController.java
@@ -0,0 +1,11 @@
+package com.whoamizq.rabbitmqmail.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/token")
+public class TokenController {
+
+
+}
---
+++ src/main/java/com/whoamizq/rabbitmqmail/service/impl/TokenServiceImpl.java
@@ -0,0 +1,9 @@
+package com.whoamizq.rabbitmqmail.service.impl;
+
+import com.whoamizq.rabbitmqmail.service.TokenService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenServiceImpl implements TokenService {
+
+}
