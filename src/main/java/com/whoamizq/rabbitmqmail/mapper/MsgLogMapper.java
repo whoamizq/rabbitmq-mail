7064ef61f2d72b8fb484a27c38538229227ed5a5
zhangzhenQuan <1726891762@qq.com>
2020-10-19 16:27:17 +0800

    添加MsgLog插入sql

--- src/main/java/com/whoamizq/rabbitmqmail/mapper/MsgLogMapper.java
+++ src/main/java/com/whoamizq/rabbitmqmail/mapper/MsgLogMapper.java
@@ -4,4 +4,6 @@ import com.whoamizq.rabbitmqmail.pojo.MsgLog;
 import com.whoamizq.rabbitmqmail.service.batch.BatchProcessMapper;
 
 public interface MsgLogMapper extends BatchProcessMapper<MsgLog> {
+
+    void insert(MsgLog msgLog);
 }
