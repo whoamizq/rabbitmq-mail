cf24a7f0d9cee6186a46affa61a0492c7d630bee
zhangzhenQuan <1726891762@qq.com>
2020-10-20 18:04:27 +0800

    添加接口防刷限流拦截器

---
+++ src/main/java/com/whoamizq/rabbitmqmail/interceptor/AccessLimitInterceptor.java
@@ -0,0 +1,10 @@
+package com.whoamizq.rabbitmqmail.interceptor;
+
+/**
+ * @author: whoamizq
+ * @description: 接口防刷限流拦截器
+ * @date: 16:37 2020/10/20
+ **/
+public class AccessLimitInterceptor {
+    
+}
